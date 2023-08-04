43a35f1aaea38cba36b9ca2cf921a35252f9538d
heldernunes1905 <helderjnunes@gmail.com>
2023-08-04 22:24:35 +0100

    created form
    added form so user can input the city, then adds it to json file and reads it and shows info related to it

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
-<?php 
 
+<!DOCTYPE html>
+<html>
+    <body>
+        <form action="" method="post">
+            <input type="text" name="inputText"/>
+            <input type="submit" name="SubmitButton"/>
+        </form>  
+        <p id="cityname"></p>
+        <p id="temperature"></p>
+        <p id="description"></p>
+        <p id="date"></p>
+    </body>
+</html>
+
+
+
+<?php
+function sendcityname($name){
 /*$directory = dirname(__FILE__);
 $city = "London";
 echo(shell_exec("$directory/weather.py $city")); //this will call python file and send the city name*/
 $myObj = new stdClass();
-$myObj->name = "Lisbon";
-
+$myObj->name = $name;
 //echo(json_encode($myObj));
 
 
@@ -33,7 +49,60 @@ file_put_contents('info.json', $jsonData);
 $directory = dirname(__FILE__);
 shell_exec("$directory/weather.py $id"); //will send the id that was inputted so it knows what to change
 
+return $id;
+}
 
+
+?>
+<script>
+function getjsondata(idadd){
+    
+    var myData;
+
+    fetch("./info.json")
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        myData = data; 
+        showdata(myData,idadd);
+    });
+}
+
+function showdata(data,id){
+    //console.log(id-1);
+    ogid = id;
+    id = id - 1;
+    current = data[id];
+
+    const object = JSON.parse(current[ogid]);
+    const name = object["name"];
+    const temp = object["temp"];
+    const desc = object["desc"];
+    const date = object["date"];
+    
+    document.getElementById("cityname").textContent += "City: " + name;
+    document.getElementById("temperature").textContent += "Temperature: " + temp;
+    document.getElementById("description").textContent += "Description: " + desc;
+    document.getElementById("date").textContent += "Date: " + date;
+
+}
+
+</script>
+<?php
+$message = "";
+if(isset($_POST['SubmitButton'])){ //check if form was submitted
+  $input = $_POST['inputText']; //get input text
+  $idadded = sendcityname($input);
+  
+  ?>
+     <script>
+        
+        getjsondata(<?php echo $idadded ?>);
+        
+    </script> 
+  <?php
+}    
 ?>
 
 
